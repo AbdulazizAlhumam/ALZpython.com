ec6a987b7ee1df1b3c38b5645a718b9c96f5ab89
AbdulazizAlhumam <86843620+AbdulazizAlhumam@users.noreply.github.com>
2021-12-16 20:43:54 +0300

    update

---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    bool learningCpp = true;
+
+    if (learningCpp == true){
+        cout << "lets make some projects";
+    }
+    else{
+        cout << "Lets learn C++";
+    }
+}
